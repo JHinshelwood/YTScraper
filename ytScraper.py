5fa7d2806122d486659ad5b508e98a9d394c3279
Jack Hinshelwood <jhinshelwood96@gmail.com>
2018-02-27 18:18:03 +0000

    Add print statement for subtitle link

--- ytScraper.py
+++ ytScraper.py
@@ -14,7 +14,7 @@ def create_link():
         video_code = url.split(v,1)[1]
         global vc
         vc = video_code
-        print(subtitle_link + video_code)
+        print("Loading subtitles from: " + subtitle_link + video_code)
         return subtitle_link + video_code
     else:
         print("Sorry - I don't seem to recognise that link, please try again.")
@@ -34,10 +34,13 @@ soup = BeautifulSoup(html, "lxml")
 print("")
 results = soup.find_all(string=re.compile(create_search_term(), re.I))
 print("")
+
+text_str = "";
 for element in results:
     parent_e = element.parent
     time = parent_e['start']
     sentence = str(parent_e.text).replace('&#39;', '\'')
+    text_str += sentence
 
     time_string = str(time)
     int_time = time_string.split(".",1)[0]
